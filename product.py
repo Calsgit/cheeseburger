d969cf09536d34060490be3fcbd7dc2eda5ac4dd
Calsgit <agen7c@gmail.com>
2025-11-11 09:47:28 +1000

    add __init__ method to product class

--- product.py
+++ product.py
@@ -1,2 +1,6 @@
 class Product:
-    """Defines a new product class."""
+    """Define a new product class."""
+    def __init__(self, name="New product", price=0.00):
+        """Initialise the class."""
+        self.name = name
+        self.price = price
