f96d3ed34572197468f96fade1000c28aa5f260b
Calsgit <agen7c@gmail.com>
2025-11-11 10:09:26 +1000

    Add some scaffolds to main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 """Main thing"""
+from product import Product
+
+MAIN_FILE = "products.json"
+
+def main():
+    """Add something here!!"""
+    print("Something here")
+
+
+def load_products():
+    """Load products from json format"""
+    pass # don't use this yet
