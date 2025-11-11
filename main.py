804544296986da9e9a0f58bbb4216b34c9807052
Calsgit <agen7c@gmail.com>
2025-11-11 10:36:26 +1000

    Add more kivy layout and runnable main function

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ MAIN_FILE = "products.json"
 
 def main():
     """Add something here!!"""
-    print("Something here")
+    kivything().run()
 
 
 def load_products():
     """Load products from json format"""
     pass # don't use this yet
+
+if __name__ == '__main__':
+    main()
